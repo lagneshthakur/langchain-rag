a5d4fc34e8a29dc3148b707adacb7df2347db884
Lagnesh Thakur <lagneshthakur@gmail.com>
2025-01-08 16:55:36 +0530

    removes extra print statements

--- create_database.py
+++ create_database.py
@@ -48,9 +48,6 @@ def split_text(documents: list[Document]):
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
 
-    document = chunks[10]
-    print(document.page_content)
-    print(document.metadata)
 
     return chunks
 
